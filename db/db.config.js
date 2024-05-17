b9a1b7b4113102f1d85581e2f28e435140a667d0
OranFrydman <oranfry@post.bgu.ac.il>
2024-05-17 17:15:33 +0300

    config DB env

--- db/db.config.js
+++ db/db.config.js
@@ -1,7 +1,15 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "oMate5357#",
-    DB: "mysql"
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "oMate5357#",
+//     DB: "mysql"
     
-    };
+//     };
+
+module.exports = {
+        HOST: process.env.HOST || "localhost",
+        USER: process.env.USER || "root",
+        PASSWORD: process.env.PASSWORD || "oMate5357#",
+        DB: process.env.DB || "mysql"
+        
+        };
